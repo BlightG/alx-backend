6b0dcb05140c850faf4b9048c64639b6f9243b7d
BlightG <birhane.gulilat@gmail.com>
2023-05-23 11:14:25 +0300

    task 1 first attempt

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -9,6 +9,8 @@ class LFUCache(BaseCaching):
 
     # to be used for storing values and use order
     use_dict = {}
+    frequency_dict = {}
+    min_LFU = 1
 
     def __init__(self):
         """ initalizaes the LFU cache class """
@@ -28,7 +30,7 @@ class LFUCache(BaseCaching):
                         self.cache_data.pop(k)
                         print('DISCARD: {}'.format(k))
                         break
-            self.LFU(key)
+            self.LRU(key)
             self.cache_data[key] = item
 
     def get(self, key):
@@ -42,35 +44,43 @@ class LFUCache(BaseCaching):
 
         try:
             item = self.cache_data[key]
-            self.LFU(key)
+            self.LRU(key)
             return item
         except KeyError:
             return None
 
-    def LFU(self, key):
+    def LRU(self, key):
         """ logs the usecase for keys in the use_dict """
 
         if key is None:
             return None
 
+        # if new key
         if key not in self.use_dict.keys():
+            # if chache is full
             if len(self.use_dict) >= self.MAX_ITEMS:
                 for k in self.use_dict.keys():
+                    # find the LRU key
                     if self.use_dict[k] == self.MAX_ITEMS:
+                        # remove LRU key
                         self.use_dict.pop(k)
+                        # insert new one with rank 1 to indicate its new
                         self.use_dict[key] = 1
                         break
             else:
+                # if cache isnt full simply add new key
                 self.use_dict[key] = 1
+            # adjust rank of all remaining keys
             for k in self.use_dict.keys():
                 if k != key:
                     self.use_dict[k] += 1
         else:
+            # if not new key
+            # save rank of the key before chaning it to 1
             key_rank = self.use_dict[key]
             self.use_dict[key] = 1
+            # change rank for all rank smaller than previous rank of key
             for k in self.use_dict.keys():
                 if k != key:
                     if self.use_dict[k] < key_rank:
                         self.use_dict[k] += 1
-
-        # print(f'use_dict = {self.use_dict}')
